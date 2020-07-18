923cfeab5cd00e4e16e11ba6463de3bd8d6b960a
Hannes <hasnep@gmail.com>
2020-07-18 13:15:33 +0100

    Add workin puzzle solver example
    Only for + operations

--- src/KenKen.jl
+++ src/KenKen.jl
@@ -1,141 +1,110 @@
+# # KenKen
 
-# Define a simple problem
-kenken_size = 3
-kenken_cages = [
-    Dict(:cells => [(1, 1), (2, 1)], :value => 5, :operation => +),
-    Dict(:cells => [(1, 2), (1, 3)], :value => 3, :operation => +),
-    Dict(:cells => [(2, 2)], :value => 3, :operation => +),
-    Dict(:cells => [(2, 3), (3, 3)], :value => 4, :operation => +),
-    Dict(:cells => [(3, 1), (3, 2)], :value => 3, :operation => +),
-]
+# ## Introduction
 
-# Define a harder problem
-# kenken_size = 5
-# kenken_cages = [
-#     Dict(:cells => [(1, 1), (2, 1), (3, 1)], :value => 11, :operation => +),
-#     Dict(:cells => [(1, 2), (2, 2), (3, 2)], :value => 6, :operation => +),
-#     Dict(:cells => [(1, 3), (1, 4), (2, 3)], :value => 9, :operation => +),
-#     Dict(:cells => [(1, 5)], :value => 2, :operation => +),
-#     Dict(:cells => [(2, 4), (2, 5), (3, 5), (4, 5)], :value => 12, :operation => +),
-#     Dict(:cells => [(3, 3), (3, 4), (4, 4)], :value => 11, :operation => +),
-#     Dict(:cells => [(4, 1), (4, 2), (4, 3), (5, 2)], :value => 14, :operation => +),
-#     Dict(:cells => [(5, 1)], :value => 1, :operation => +),
-#     Dict(:cells => [(5, 3), (5, 4), (5, 5)], :value => 9, :operation => +),
-# ]
+# [KenKen](https://en.wikipedia.org/wiki/KenKen) is a number puzzle similar to the Sodoku where the goal is to place the digits 1 to ``N`` on an ``N \times N`` grid, with some of the digits already filled in.
 
-# Define a hard problem
-# kenken_size = 6
-# kenken_cages = [
-#     Dict(:cells => [(1, 1)], :value => 1, :operation => +),
-#     Dict(:cells => [(2, 1), (1, 2), (2, 2)], :value => 15, :operation => +),
-#     Dict(:cells => [(3, 1), (4, 1), (3, 2)], :value => 10, :operation => +),
-#     Dict(:cells => [(5, 1), (4, 2), (5, 2), (4, 3)], :value => 13, :operation => +),
-#     Dict(:cells => [(6, 1), (6, 2), (6, 3)], :value => 11, :operation => +),
-#     Dict(:cells => [(1, 3), (2, 3), (1, 4), (1, 5)], :value => 13, :operation => +),
-#     Dict(:cells => [(3, 3), (3, 4), (3, 5)], :value => 11, :operation => +),
-#     Dict(:cells => [(5, 3), (5, 4), (6, 4)], :value => 7, :operation => +),
-#     Dict(:cells => [(2, 4), (2, 5)], :value => 7, :operation => +),
-#     Dict(:cells => [(4, 4), (4, 5), (4, 6)], :value => 11, :operation => +),
-#     Dict(:cells => [(5, 5), (6, 5), (5, 6)], :value => 13, :operation => +),
-#     Dict(:cells => [(1, 6), (2, 6), (3, 6)], :value => 9, :operation => +),
-#     Dict(:cells => [(6, 6)], :value => 5, :operation => +),
-# ]
+# Our solution must satisfy the following rules:
+#   * The digits 1 to ``N`` must appear once each in each row.
+#   * The digits 1 to ``N`` must appear once each in each column.
+#   * The digits in each shape outlined by thicker lines (called a cage) must equal the number in the corner of the cage when combined using the cage's operation.
 
-# # Define a super hard problem
-# kenken_size = 9
-# kenken_cages = [
-#     Dict(:cells => [(1, 1), (1, 2)], :value => 54, :operation => *),
-#     Dict(:cells => [(2, 1), (3, 1), (4, 1), (5, 1)], :value => 10, :operation => +),
-#     Dict(:cells => [(6, 1), (7, 1), (8, 1)], :value => 20, :operation => +),
-#     Dict(:cells => [(9, 1), (9, 2)], :value => 5, :operation => -),
-#     Dict(:cells => [(2, 2), (3, 2)], :value => 2, :operation => /),
-#     Dict(:cells => [(4, 2), (5, 2)], :value => 3, :operation => -),
-#     Dict(:cells => [(6, 2), (6, 3), (6, 4)], :value => 20, :operation => *),
-#     Dict(:cells => [(7, 2), (8, 2)], :value => 5, :operation => -),
-#     Dict(:cells => [(1, 3), (2, 3)], :value => 8, :operation => -),
-#     Dict(:cells => [(3, 3), (4, 3), (5, 3)], :value => 168, :operation => *),
-#     Dict(:cells => [(7, 3), (7, 4)], :value => 1, :operation => -),
-#     Dict(:cells => [(8, 3), (9, 3)], :value => 2, :operation => /),
-#     Dict(:cells => [(1, 4), (1, 5)], :value => 15, :operation => *),
-#     Dict(:cells => [(2, 4), (3, 4)], :value => 5, :operation => -),
-#     Dict(:cells => [(4, 4), (4, 5)], :value => 72, :operation => *),
-#     Dict(:cells => [(5, 4), (5, 5)], :value => 8, :operation => -),
-#     Dict(:cells => [(8, 4), (8, 5)], :value => 3, :operation => /),
-#     Dict(:cells => [(9, 4), (9, 5)], :value => 2, :operation => -),
-#     Dict(:cells => [(2, 5), (3, 5), (3, 6), (3, 7)], :value => 22, :operation => +),
-#     Dict(:cells => [(6, 5), (6, 6)], :value => 4, :operation => -),
-#     Dict(:cells => [(7, 5)], :value => 4, :operation => +),
-#     Dict(:cells => [(1, 6), (1, 7)], :value => 3, :operation => -),
-#     Dict(:cells => [(2, 6), (2, 7)], :value => 4, :operation => /),
-#     Dict(:cells => [(4, 6), (4, 7)], :value => 2, :operation => /),
-#     Dict(:cells => [(5, 6), (5, 7)], :value => 2, :operation => -),
-#     Dict(:cells => [(7, 6), (8, 6)], :value => 3, :operation => /),
-#     Dict(:cells => [(8, 6)], :value => 5, :operation => +),
-#     Dict(:cells => [(6, 7), (7, 7), (7, 8)], :value => 16, :operation => +),
-#     Dict(:cells => [(8, 7), (9, 7)], :value => 5, :operation => -),
-#     Dict(:cells => [(1, 8), (1, 9), (2, 9)], :value => 80, :operation => *),
-#     Dict(:cells => [(2, 8), (3, 8), (3, 9)], :value => 14, :operation => +),
-#     Dict(:cells => [(4, 8), (5, 8), (6, 8)], :value => 10, :operation => +),
-#     Dict(:cells => [(8, 8), (8, 9)], :value => 3, :operation => /),
-#     Dict(:cells => [(9, 8), (9, 9)], :value => 6, :operation => -),
-#     Dict(:cells => [(4, 9), (5, 9), (6, 9), (7, 9)], :value => 30, :operation => +),
-# ]
+# We'll define some structs to hold information about the puzzle and the cages.
 
+struct Cage
+    cells::Vector{Tuple{Integer, Integer}}
+    operation::Function
+    target::Number
+end
+
+struct Puzzle
+    size::Integer
+    cages::Vector{Cage}
+end
+
+# Define an easy ``3 \times 3`` puzzle using only the ``+`` operator.
+
+puzzle = Puzzle(
+    3,
+    [
+        Cage([(1, 1), (2, 1)], +, 5),
+        Cage([(1, 2), (1, 3)], +, 3),
+        Cage([(2, 2)], +, 3),
+        Cage([(2, 3), (3, 3)], +, 4),
+        Cage([(3, 1), (3, 2)], +, 3),
+    ],
+)
+
+# ## Solver
+
+# We will use the integer solver from [Cbc.jl](https://github.com/JuliaOpt/Cbc.jl) for this example.
 
 using JuMP
 using Cbc
 
+# This isn't an optimization problem, it's actually a feasibility problem.
+# We want to find any solution that satsifies the rules of the puzzle, so we set `maxSolutions` to 1 so that the solver stops as soon as it finds a solution.
 
-# Create a JuMP optimiser
+## Create a JuMP optimiser
 kenken = Model(with_optimizer(Cbc.Optimizer, maxSolutions = 1))
 
+# We will define a binary variable for each possible number in each possible cell, i.e., the variable ``x_{i, j, k}`` will be 1 if the cell at position ``(i, j)`` contains the digit ``k`` and 0 otherwise.
 
-@variable(kenken, x[i = 1:kenken_size, j = 1:kenken_size, k = 1:kenken_size], Bin)
+@variable(kenken, x[i = 1:(puzzle.size), j = 1:(puzzle.size), k = 1:(puzzle.size)], Bin)
 
+# Now we can add out first constraint: that each cell must contain exactly one digit.
+# For each cell at position ``(i, j)``, the sum across all possible digits must be 1.
 
-for i in 1:kenken_size, j in 1:kenken_size
-    @constraint(kenken, sum(x[i, j, k] for k in 1:kenken_size) == 1)
+for i in 1:(puzzle.size), j in 1:(puzzle.size)
+    @constraint(kenken, sum(x[i, j, digit] for digit in 1:(puzzle.size)) == 1)
 end
 
+# Next we'll add the constraints that each row and column must contain each digit once.
+# We loop over all the columns using the variable `index` and over all possible digits using `digit`.
+# In each value of `index`, we constrain that column to only contains the digit `digit` once.
+# The constraints for the rows are very similar, so we can add them at the same time.
 
-
-# for Each row, OR each column and for Each digit
-for index in 1:kenken_size, k in 1:kenken_size
-        # Sum across columns 
-        @constraint(kenken, sum(x[index, j, k] for j in 1:kenken_size) == 1)
-        # Sum across rows 
-        @constraint(kenken, sum(x[i, index, k] for i in 1:kenken_size) == 1)
+for index in 1:(puzzle.size), digit in 1:(puzzle.size)
+    ## The sum over each column must be 1 
+    @constraint(kenken, sum(x[index, j, digit] for j in 1:(puzzle.size)) == 1)
+    ## The sum over each row must be 1 
+    @constraint(kenken, sum(x[i, index, digit] for i in 1:(puzzle.size)) == 1)
 end
 
-# Constrain the cages
-for cage in kenken_cages
+# Finally, we have to make sure that the digits in each cage combine to that cage's target number.
+# For each cage we'll use the `reduce` function to combine the digits in the cage's cells.
+
+# TODO: explain this more
+
+# We constrain this to be equal to the cage's target value.
+
+for cage in puzzle.cages
     @constraint(
         kenken,
         reduce(
-            cage[:operation],
-            k * x[i, j, k] for (i, j) in cage[:cells], k in 1:kenken_size
-        ) == cage[:value]
+            cage.operation,
+            [x[i, j, k] * k for (i, j) in cage.cells for k in 1:(puzzle.size)],
+        ) == cage.target
     )
 end
 
-@info "Solving"
+# Now we have all of the constraints in place we can solve the problem!
+
 optimize!(kenken)
-@info "Solved"
 
-# Extract the values of x
-# Integer programs are solved as a series of linear programs
-# so the values might not be precisely 0 and 1. We can just
-# round them to the nearest integer to make it easier
-x_solution = round.(Integer, value.(x))
+# Extract the values of ``x``.
+# Integer programs are solved as a series of linear programs so the values might not be precisely 0 and 1.
+# We can just round them to the nearest integer to fix this.
 
-# Create a matrix to store the solution
-matrix_solution = zeros(Int, kenken_size, kenken_size)  # 9x9 matrix of integers
-for i in 1:kenken_size, j in 1:kenken_size, k in 1:kenken_size
+x_solution = round.(Int, value.(x))
+
+# To display the solution as a matrix we need to look for the values of `x[i, j, k]` that are equal to 1.
+
+matrix_solution = zeros(Int, puzzle.size, puzzle.size)
+for i in 1:(puzzle.size), j in 1:(puzzle.size), k in 1:(puzzle.size)
     if x_solution[i, j, k] == 1
         matrix_solution[i, j] = k
     end
 end
 
-# Display the solution
 matrix_solution
-
